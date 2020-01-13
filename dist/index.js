6d8215c87d4307b236a7079db7b079e86a1e57ea
Silva <eduardo.felipe.silva@accenture.com>
2020-01-13 15:03:45 -0300

    was added a webpack umd generate and example files

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,18 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-class ExtensibleJsPDF {
-    constructor() { }
+const jspdf_1 = __importDefault(require("jspdf"));
+class ExtensibleJsPDF extends jspdf_1.default {
+    constructor(options, styles) {
+        super(options);
+        this.styles = styles;
+        this.y = 0;
+        this.setPageLimits = () => {
+            console.log('here', this.internal.getCurrentPageInfo());
+        };
+        this.setPageLimits();
+    }
 }
 exports.default = ExtensibleJsPDF;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
-export default class ExtensibleJsPDF {
-  constructor() {}
+import jsPDF from 'jspdf';
+
+export default class ExtensibleJsPDF extends jsPDF {
+  private y = 0;
+
+  constructor(options: jsPDF, private styles: object) {
+    super(options);
+    this.setPageLimits();
+  }
+
+  private setPageLimits = () => {
+    console.log('here', this.internal.getCurrentPageInfo());
+  }
+  
 }
